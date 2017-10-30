1c66d0bfcb235663241523ac4b38dcbe153b8fbb
Nuno Alexandre <nunnomalex@gmail.com>
2017-10-30 12:28:38 +0000

    user related stuff

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->rememberToken();
             $table->string('nickname')->unique();
             $table->boolean('admin')->default(false);
             $table->boolean('blocked')->default(false);
